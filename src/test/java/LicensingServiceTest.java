4c99f0ef600351ec71ec9740df5b5a9ccc93a55a
Ash <apillai@buffalo.edu>
2022-09-13 10:58:08 -0700

    Add files via upload

---
+++ src/test/java/LicensingServiceTest.java
@@ -0,0 +1,8 @@
+public class LicensingServiceTest {
+
+    //@Test
+    public void licenseEligibilityChecker() {
+        LicensingService licensingService = new LicensingService();
+        licensingService.eligibilityChecker(1);
+    }
+}
